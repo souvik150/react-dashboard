b77bf45e775d6463e058de5b8709ac7afc88549f
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-12-03 12:32:25 +0530

    feat: adds hero section -> ecommerce page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
-const Button = () => {
+// import { useStateContext } from '../context/ContextProvider';
+
+const Button = ({ icon, bgColor, color, bgHoverColor, size, text, borderRadius, width }) => {
+    // const { setIsClicked, initialState } = useStateContext();
+
     return (
-        <div>Button</div>
+        <button
+            type="button"
+            // onClick={() => setIsClicked(initialState)}
+            style={{ backgroundColor: bgColor, color, borderRadius }}
+            className={` text-${size} p-3 hover:drop-shadow-xl`}
+        >
+            {text}
+            {/*{icon} {text}*/}
+        </button>
     );
-}
+};
 
 export default Button;
